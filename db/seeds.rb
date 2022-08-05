1cf258b37fa5fd1d1da20fdbcd1465ff8023c3e5
Artem Vetrov <artem.vetrov@yieldbird.com>
2022-08-05 16:11:20 +0300

    feat: modify seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,15 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-2.times do
+1.times do
   ActiveRecord::Base.connection.execute <<-SQL
     INSERT INTO users (name,country,score,created_at,updated_at) VALUES
     #{UserGenerator.generate(100_000)}
   SQL
 end
+
+ActiveRecord::Base.connection.execute <<-SQL
+  CREATE INDEX users_name ON users(name);
+  CREATE INDEX users_country ON users(country);
+  CREATE INDEX users_score ON users(score);
+SQL
