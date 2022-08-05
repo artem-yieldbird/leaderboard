5b8bfef08ca460a52a8a4c38da5f2c9cb77c25b9
Artem Vetrov <artem.vetrov@yieldbird.com>
2022-08-05 17:29:35 +0300

    feat: customize users index page

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -3,15 +3,9 @@
 ActiveAdmin.register User do
   permit_params :title, :body
 
-  show do
-    attributes_table(:body, :created_at)
-
-    panel 'Users' do
-      table_for User do
-        column(:name)
-        column(:country)
-        column(:score)
-      end
-    end
+  index do
+    column :name
+    column :country
+    column :score
   end
 end
